47b0a22d59e893efa140e3dc48fb486f64d4429c
vinh9999hiih <hoangvinh09062008@gmail.com>
2025-03-08 21:18:12 +0700

    functions.py

---
+++ functions.py
@@ -0,0 +1,12 @@
+# functions.py
+def show_message():
+    """Hiển thị thông báo."""
+    return "Đây là chức năng hiển thị thông báo!"
+
+def calculate_sum(num1, num2):
+    """Tính tổng hai số."""
+    return num1 + num2
+
+def count_characters(text):
+    """Đếm số ký tự trong chuỗi."""
+    return len(text)
